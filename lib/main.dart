09213169d3a807e09f330b4bb9194757da90e918
Sanjay Singha <make.sanjoy@gmail.com>
2022-11-10 19:13:51 +0530

    adding files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter30days/home_page.dart';
+import 'package:flutter30days/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      home:  HomePage(),
+    );
+
   }
 }
 
